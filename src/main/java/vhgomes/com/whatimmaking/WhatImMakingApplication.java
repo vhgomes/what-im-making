ffb49caef26c754add5e14b95e47a27b82adfb43
Victor Hugo <vhclaudiogomes@gmail.com>
2024-05-07 12:05:49 -0300

    initial commit

---
+++ src/main/java/vhgomes/com/whatimmaking/WhatImMakingApplication.java
@@ -0,0 +1,13 @@
+package vhgomes.com.whatimmaking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatImMakingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatImMakingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vhgomes/com/whatimmaking/WhatImMakingApplicationTests.java
@@ -0,0 +1,13 @@
+package vhgomes.com.whatimmaking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatImMakingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
